a3ae094a4409c1d6f1ed818e777bbda00eef0806
Alexandra Matveeva <alexa.matveeva.dev@gmail.com>
2022-07-12 16:48:25 +0300

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Alexandra on 12.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
